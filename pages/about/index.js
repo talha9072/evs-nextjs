6c3946fc9fbff9aeb40ee69a82e284592f8d018a
talha9072 <130919309+talha9072@users.noreply.github.com>
2025-12-22 13:36:40 +0500

    up1

--- pages/about/index.js
+++ pages/about/index.js
@@ -1005,6 +1005,11 @@ export default function About() {
         </div>
       </section>
           
+           <div class="scroll-progress d-none d-xxl-block">
+            <a href="#" class="scroll-top" aria-label="scroll">
+                <span class="scroll-text">Scroll</span><span class="scroll-line"><span class="scroll-point"></span></span>
+            </a>
+        </div>
 
     </>
   );
