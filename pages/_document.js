96bad09c65ba445d355d1c246ebc288959281125
talha9072 <130919309+talha9072@users.noreply.github.com>
2026-01-13 13:30:52 +0500

    gtm fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         {/* ===== BASE CSS ===== */}
         <link rel="stylesheet" href="/css/vendors.min.css" />
@@ -12,10 +12,33 @@ export default function Document() {
         <link rel="stylesheet" href="/css/responsive.min.css" />
         <link rel="stylesheet" href="/css/evs.css" />
 
-        
+        {/* ===== GOOGLE TAG MANAGER ===== */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NT2KKJB');
+            `,
+          }}
+        />
       </Head>
 
       <body>
+        {/* ===== GTM NOSCRIPT (IMMEDIATELY AFTER <body>) ===== */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NT2KKJB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
